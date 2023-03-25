2bb3f0c99146a193c0d2732de2ff23b5709027b3
Daniel <edanielarredondo@gmail.com>
2023-03-25 03:15:04 -0500

    Add delete workout feature

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 
 const WorkoutDetails = ({ workout }) => {
+
+    const { dispatch } = useWorkoutsContext();
+
+    const handleClick = async () => {
+        const response = await fetch(`/api/workouts/${workout._id}`, {
+            method: 'DELETE'
+        });
+        const json = await response.json();
+
+        if (response.ok) {
+            console.log(json.result);
+            dispatch({type: 'REMOVE_WORKOUT', payload: json.result});
+        }
+    };
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (lbs): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     );
 };
